295ce24433f71cf5df5f68003e2c8edb2bdfb3bd
Alexseybond <kidai_cuda@mail.ru>
2023-02-12 21:29:20 +0300

    add option to choose languages

--- conftest.py
+++ conftest.py
@@ -10,20 +10,29 @@ def pytest_addoption(parser):
                      action='store',
                      default="chrome",
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language',
+                     action='store',
+                     default="en",
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         # options.add_argument('headless')
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
         os.environ['MOZ_HEADLESS'] = '1'
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
--- test_main_page.py
+++ test_main_page.py
@@ -1,26 +1,30 @@
+import pytest
+
 from pages.basket_page import BasketPage
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.locators import MainPageLocators
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = MainPageLocators.URL
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.should_be_login_link()
-    main_page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
 
+    def test_guest_can_go_to_login_page(self, browser):
+        link = MainPageLocators.URL
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.should_be_login_link()
+        main_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = MainPageLocators.URL
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.should_be_basket_link()
-    main_page.go_to_basket_page()
-    basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_basket_page()
-    basket_page.should_be_basket_is_empty()
+    def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
+        link = MainPageLocators.URL
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.should_be_basket_link()
+        main_page.go_to_basket_page()
+        basket_page = BasketPage(browser, browser.current_url)
+        basket_page.should_be_basket_page()
+        basket_page.should_be_basket_is_empty()
 
